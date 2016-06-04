2d13428ba0d549c8f3f81f098ca3623160a406d6
Facundo Olano <facundo.olano@gmail.com>
2016-06-04 17:33:22 -0300

    all functions supporting both stores

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 'use strict';
 
-module.exports.app = require('./lib/app');
-module.exports.scores = require('./lib/scores');
-module.exports.suggest = require('./lib/suggest');
+const gplay = require('./lib/stores/gplay');
+const itunes = require('./lib/stores/itunes');
+
+const buildApp = require('./lib/app');
+const buildScores = require('./lib/scores');
+const buildSuggest = require('./lib/suggest');
+
+module.exports.gplay = {
+  app: buildApp(gplay),
+  scores: buildScores(gplay),
+  suggest: buildSuggest(gplay)
+};
+
+module.exports.itunes = {
+  app: buildApp(itunes),
+  scores: buildScores(itunes),
+  suggest: buildSuggest(itunes)
+};
