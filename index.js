390f00dc72e790e86ac1f199d840f030344183be
Facundo Olano <facundo.olano@gmail.com>
2016-06-18 12:34:24 -0300

    cleanup visibility

--- index.js
+++ index.js
@@ -7,15 +7,18 @@ const constants = require('./lib/constants');
 const buildApp = require('./lib/app');
 const buildScores = require('./lib/scores');
 const buildSuggest = require('./lib/suggest');
+const buildVisibility = require('./lib/visibility');
 
 module.exports.gplay = Object.assign({
   app: buildApp(gplay),
   scores: buildScores(gplay),
-  suggest: buildSuggest(gplay)
+  suggest: buildSuggest(gplay),
+  visibility: buildVisibility(gplay)
 }, constants);
 
 module.exports.itunes = Object.assign({
   app: buildApp(itunes),
   scores: buildScores(itunes),
-  suggest: buildSuggest(itunes)
+  suggest: buildSuggest(itunes),
+  visibility: buildVisibility(itunes)
 }, constants);
