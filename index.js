4ecb6a1d2026dbbdbbdd3be94054054b7434acfb
Facundo Olano <facundo.olano@gmail.com>
2018-11-19 14:02:27 -0300

    update scraper dependencies (#19)
    * update scraper dependencies
    
    * fix days since for google play
    
    * remove console.log
    
    * update app-store-scraper package
    
    * fix throttle property
    
    * add debug to scraper calls
    
    * updated app store scraper

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 require('promise-log')(Promise);
+const R = require('ramda');
 
 const STORES = {
   'gplay': require('./lib/stores/gplay'),
@@ -14,17 +15,23 @@ const buildScores = require('./lib/scores');
 const buildSuggest = require('./lib/suggest');
 const buildVisibility = require('./lib/visibility');
 
-function getClient (store, opts) {
-  opts = Object.assign({
-    country: 'us',
-    throtle: 20
-  }, opts);
 
-  if (!(store in STORES)) {
-    throw Error(`the store name should be one of: ${Object.keys(STORES).join(', ')}`);
+function getClient (store, opts) {
+  // not forcing store to be a string anymore, in case someone wanats to pass
+  // a homebrew object, e.g. to disable memoization
+
+  if (R.is(String, store)) {
+    opts = Object.assign({
+      country: 'us',
+      throttle: 20
+    }, opts);
+    if (!(store in STORES)) {
+      throw Error(`the store name should be one of: ${Object.keys(STORES).join(', ')}`);
+    }
+
+    store = STORES[store](opts);
   }
 
-  store = STORES[store](opts);
   return Object.assign({
     app: buildApp(store),
     scores: buildScores(store),
